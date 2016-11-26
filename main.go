b9d9db3f657e188401cf979db82ff225f378be24
diogoalmiro <diogoalmiro@gmail.com>
2016-11-26 12:48:44 +0000

    Learning git go

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello world!")
+	for i := 12; i >= 0; i-- {
+		if i != 10 && i != 4 && i != 2 {
+			fmt.Println("i =", i)
+		}
+	}
+	recursionFTW(12)
+}
+
+func recursionFTW(i int) {
+	if i != 10 && i != 4 && i != 2 {
+		fmt.Println("i =", i)
+	}
+	if i == 0 {
+		return
+	}
+	recursionFTW(i - 1)
 }
